a5729c2412dc147c017470f938d49ef941503049
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-05-03 15:58:07 -0500

    use processing logger on finalizer

--- proto/rethink/finalizer.zig
+++ proto/rethink/finalizer.zig
@@ -2,6 +2,8 @@ const std = @import("std");
 const types = @import("types.zig");
 
 pub const Finalizer = struct {
+    logger:@import("logger.zig").Logger = .init,
+
     pub fn init(_:std.mem.Allocator) !Finalizer {
         return .{};
     }
@@ -11,9 +13,19 @@ pub const Finalizer = struct {
         block:*types.Block,
         parent:?*types.Block
     ) !types.Block {
+        try self.logger.task(.start, "finalizer.recurse(\"{s}\", \"{s}\")", .{
+            block.name orelse "[unlabeled block]",
+            if (parent) |p| p.name orelse "[unlabeled block]" else "[root]"
+        });
+        defer self.logger.task(.done, "finalizer.recurse(\"{s}\", \"{s}\")", .{
+            block.name orelse "[unlabeled block]",
+            if (parent) |p| p.name orelse "[unlabeled block]" else "[root]"
+        }) catch {};
+
         var block_itr = block.namespace.iterator();
         while (block_itr.next()) |entry| {
-            _ = switch (entry.value_ptr.*.type) {
+            var value = entry.value_ptr.*;
+            _ = switch (value.tok.type) {
                 .block => |*blk| try self.recurse(blk, block),
                 else => {},
             };
@@ -23,13 +35,17 @@ pub const Finalizer = struct {
             while (parent_itr.next()) |entry| {
                 const name = @constCast(entry.key_ptr.*);
                 const value = entry.value_ptr.*;
-                try block.to_namespace(name, value);
+                try self.logger.task(.operation, "from parent:|{s}|", .{name});
+                defer self.logger.task(.done, "from parent:|{s}|", .{name}) catch {};
+                try block.to_namespace(name, value.changeable, value.tok);
             }
         }
         return block.*;
     }
 
     pub fn do(self:*Finalizer, block:*types.Block) !types.Block {
+        try self.logger.stage(.start, "finalizer", .{});
+        defer self.logger.stage(.done, "finalizer", .{}) catch {};
         return try self.recurse(block, null);
     }
 };
